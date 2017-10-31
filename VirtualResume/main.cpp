0c8c824ec3fbd4b4bff6fab0e19893860c5af8f6
Lucien <LucienXian97@gmail.com>
2017-10-31 09:26:02 +0800

    init window

---
+++ VirtualResume/main.cpp
@@ -0,0 +1,24 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "shader.h"
+#include <iostream>
+
+void InitWindow()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+
+int main()
+{
+	InitWindow();
+}
