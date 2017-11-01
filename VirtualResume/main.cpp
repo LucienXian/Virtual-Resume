812507bc9faa3c165286a777f25c173b7bbe14d7
Lucien <LucienXian97@gmail.com>
2017-11-01 19:19:36 +0800

    show the resume

--- VirtualResume/main.cpp
+++ VirtualResume/main.cpp
@@ -3,6 +3,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #include <string>
+#include <vector>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -18,6 +19,8 @@ const std::string window_name = "Resume";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+void load_texture(GLuint &texture, const std::string& path);
+void add_texture(std::vector<GLuint>& v);
 
 void InitWindow()
 {
@@ -52,7 +55,7 @@ int main()
 
 	GLfloat vertices[] = {
 		//     ---- 位置 --     - 纹理坐标 -
-		0.6f,  0.6f, 0.2f,    1.0f, 1.0f,   // 右上
+		0.6f,  0.6f, 0.0f,    1.0f, 1.0f,   // 右上
 		0.6f, -0.6f, 0.0f,    1.0f, 0.0f,   // 右下
 		-0.6f, -0.6f, 0.0f,    0.0f, 0.0f,   // 左下
 		-0.6f,  0.6f, 0.0f,    0.0f, 1.0f    // 左上
@@ -63,8 +66,18 @@ int main()
 		1, 2, 3  // 第二个三角形
 	};
 
-	glm::vec3 Positions[] = {
-		glm::vec3(0.0f,  0.0f,  0.0f)
+	// world space positions of our resume
+	glm::vec3 resumePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(0.2f,  0.3f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
 	};
 
 	unsigned int VBO, VAO;
@@ -89,29 +102,8 @@ int main()
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
-	unsigned int texture1;
-	glGenTextures(1, &texture1);
-	glBindTexture(GL_TEXTURE_2D, texture1);
-	// set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	// set texture filtering parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	int width, height, nrChannels;
-	unsigned char *data = stbi_load("resume/1.jpg", &width, &height, &nrChannels, 0);
-	if (data)
-	{
-		
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		std::cout << "Failed to load texture" << std::endl;
-	}
-	stbi_image_free(data);
+	std::vector<GLuint> v_texture;
+	add_texture(v_texture);
 
 	// tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
 	// -------------------------------------------------------------------------------------------
@@ -122,18 +114,42 @@ int main()
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
-
+		
 		// render
 		// ------
 		glClearColor(0, 0, 0, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // also clear the depth buffer now!
 
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture1);
-
 		
+
+		myShader.use();
+
+		glm::mat4 view;
+		glm::mat4 projection;
+		projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		// pass transformation matrices to the shader
+		myShader.setMat4("projection", projection); // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+		myShader.setMat4("view", view);
+
+
+		glBindVertexArray(VAO);
+		for (unsigned int i = 0; i < v_texture.size(); i++)
+		{
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, v_texture[i]);
+			// calculate the model matrix for each object and pass it to shader before drawing
+			glm::mat4 model;
+			model = glm::translate(model, resumePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			myShader.setMat4("model", model);
+
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		}
+
 		//glDrawArrays(GL_TRIANGLES, 0, 6);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -148,6 +164,46 @@ int main()
 	// ------------------------------------------------------------------
 	glfwTerminate();
 	return 0;
+}
+
+void add_texture(std::vector<GLuint>& v)
+{
+	
+	for (int i = 0; i < 9; i++)
+	{
+		std::string path = "resume/";
+		GLuint texture;
+		path = path + std::to_string(i + 1) + ".jpg";
+		std::cout << path << std::endl;
+		load_texture(texture, path);
+		v.push_back(texture);
+	}
+}
+
+void load_texture(GLuint &texture, const std::string& path)
+{
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	// set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int width, height, nrChannels;
+	unsigned char *data = stbi_load(path.c_str(), &width, &height, &nrChannels, 0);
+	if (data)
+	{
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
