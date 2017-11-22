c5abe33bb121d3c1958d4b7df9bf73834e08f9d4
BgmLover <787782453@qq.com>
2017-11-22 20:27:24 +0800

    修改相机朝向

--- VirtualResume/camera.h
+++ VirtualResume/camera.h
@@ -16,7 +16,7 @@ enum Camera_Movement {
 };
 
 // Default camera values
-const float YAW = -90.0f;
+const float YAW = 0.0f;
 const float PITCH = 0.0f;
 const float SPEED = 2.5f;
 const float SENSITIVTY = 0.1f;
